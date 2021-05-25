e1dc0dee65b6f398317b1316049cd248ace627c6
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-05-25 13:49:57 +1200

    Early return when moveList is empty on undo

--- src/board/undo.js
+++ src/board/undo.js
@@ -1,6 +1,7 @@
 import createBoardArray from './create-board.js';
 
 export default function undoMove() {
+	if (!global.moveList.length) return;
 	global.moveList.pop();
 	let lastMove = global.moveList.slice(-1)[0];
 	createBoardArray(lastMove);
