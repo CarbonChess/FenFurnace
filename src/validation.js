07579dd19cf186029c62d672d682758532f863f0
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-05-31 10:06:04 +1200

    Release 0.2.1 (#6)
    * log and castling bug fixes
    
    * fixed typo and added test
    
    * Release 0.2.1
    
    Co-authored-by: tenmore <61951543+akatenmore@users.noreply.github.com>

--- src/validation.js
+++ src/validation.js
@@ -134,7 +134,7 @@ export function makeMove(startCell, endCell, { isTest } = {}) {
 			pieces.move(file + row, (isKingside ? 'F' : 'D') + row); //move rook
 
 			//make castling impossible for colour that castled
-			castling[colour] = { k: false, q: false };
+			global.castling[colour] = { k: false, q: false };
 			events.castled = true;
 		}
 		else return false;
@@ -204,11 +204,11 @@ export function makeMove(startCell, endCell, { isTest } = {}) {
 
 	//add to log
 	let logText = '';
-	if (events.castled) {
+	if (events.castled === true) {
 		logText += endCell.charCodeAt(0) - startCell.charCodeAt(0) > 0 ? 'O-O' : 'O-O-O';
 	} else {
 		if (piece.toLowerCase() === 'p' && events.pieceCaptured) logText += startCell[1].toLowerCase();
-		else logText += piece.toUpperCase();
+		else if(pieces.toLowerCase !== 'p') logText += piece.toUpperCase();
 		if (events.pieceCaptured) logText += 'x';
 		logText += endCell;
 		if (events.promoted) logText += '=' + pieces.getPieceInCell(endCell).toUpperCase();
