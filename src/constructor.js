6544c9a975c9decfb2ad706aa091dd5c35d1397d
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-05-31 14:49:24 +1200

    Move pawn logic into piece class

--- src/constructor.js
+++ src/constructor.js
@@ -1,17 +1,47 @@
-import * as validation from 'validation.js';
+import pieces from './pieces.js';
 
-class CustomPiece {
+class Piece {
 
-    constructor({template, validWhen}) {
-        this.template = template;
+    constructor({ template = 'none', validWhen } = {}) {
+        this.template = template.toLowerCase();
         this.validWhen = validWhen;
     }
 
-    validate(dx,dy) {
-        switch (this.template) {
+    validate(colour, startCell, endCell) {
+        const startLetter = startCell[0];
+        const endLetter = endCell[0];
+        const deltaLetter = Math.abs(endCell.charCodeAt(0) - startCell.charCodeAt(0));
+        const startNumber = parseInt(startCell[1]);
+        const endNumber = parseInt(endCell[1]);
+        const deltaNumber = deltaNumber = Math.abs(endNumber - startNumber);
 
-            default: this.validWhen(dx,dy); break;
+        switch (this.template) {
+            case 'pawn': {
+                let invalidMove = false;
+                if (deltaLetter !== 0) return !invalidMove;
+                if (colour === 'w') {
+                    invalidMove = pieces.inCell(startLetter + (startNumber + 1));
+                    if (deltaNumber === 2 && !invalidMove) {
+                        invalidMove = pieces.inCell(startLetter + (startNumber + 2));
+                    }
+                }
+                else {
+                    invalidMove = pieces.inCell(startLetter + (startNumber - 1));
+                    if (deltaNumber === 2 && !invalidMove) {
+                        invalidMove = pieces.inCell(startLetter + (startNumber - 2));
+                    }
+                }
+                return !invalidMove;
+            }
+            default: {
+                return this.validWhen(deltaLetter, deltaNumber);
+            }
         }
     }
 
 }
+
+let pawn = new Piece({ template: 'pawn' });
+pawn.validate('white', 'E2', 'E4');
+let god = new Piece({ validWhen: () => true });
+god.validate('white', 'A1', 'H4');
