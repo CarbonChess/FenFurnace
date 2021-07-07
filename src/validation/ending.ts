33db1cc410e9dd5243d0a87020e280a0abbbd3f6
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-07-07 13:24:52 +1200

    Minor cleanup

--- src/validation/ending.ts
+++ src/validation/ending.ts
@@ -12,21 +12,18 @@ export default function gameEndingStatus(colour: Colour): EndingStatus {
 	const onlyKings = createFen().split(' ')[0].replace(/\/|\d+/g, '').toLowerCase() === 'kk';
 	if (fiftyMoveRule || onlyKings) return 'stalemate';
 
-	const currentlyCheck = isCheck(colour);
 	let noValidMoves = true;
 
 	outer:
 	for (let i = 1; i <= 8; i++) {
 		for (let j = 1; j <= 8; j++) {
 			const startCell = coordsToCell(j, i);
-			if (pieces.getColour(startCell) === colour) {
-				if (findAllMoves(startCell).length > 0)
-					noValidMoves = false;
-				if (!noValidMoves) break outer;
-			}
+			if (pieces.getColour(startCell) !== colour) continue;
+			if (findAllMoves(startCell).length > 0) noValidMoves = false;
+			if (!noValidMoves) break outer;
 		}
 	}
 
-	if (noValidMoves) return currentlyCheck ? 'checkmate' : 'stalemate';
+	if (noValidMoves) return isCheck(colour) ? 'checkmate' : 'stalemate';
 	else return false;
 }
