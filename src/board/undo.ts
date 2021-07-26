e91942d49aea8bbbfaa40ea603e8212d31799c40
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-07-26 15:10:41 +1200

    Fix move list checking

--- src/board/undo.ts
+++ src/board/undo.ts
@@ -2,7 +2,7 @@ import gameData from '../variables';
 import createBoardArray from './create-board';
 
 export default function undoMove(): string | void {
-	if (gameData.moveList.length === 0) return;
+	if (gameData.moveList.length <= 1) return;
 	gameData.moveList.pop();
 	gameData.logList.pop();
 	const lastMove = gameData.moveList[gameData.moveList.length - 1];
